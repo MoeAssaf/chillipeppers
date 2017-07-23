0f83e4d56ee7982997363151b78c7bd5589aa63d
Moe Assaf <mohammadassaf160@gmail.com>
2017-07-23 10:37:56 +0300

    first upload

--- app.py
+++ app.py
@@ -3,12 +3,16 @@ import dataset
 
 app = Flask(__name__)
 # TODO: connect your database here
-
+db=dataset.connect("postgres://fbeumtjfoswdzj:77858ede1315dab822e19dad2a8f5979e76c454512737d9598332c145f8ec4ed@ec2-50-19-89-124.compute-1.amazonaws.com:5432/d9kl1is1chajdc")
 
 @app.route('/')
 @app.route('/home')
-def homepage():
+def homepage(methods=["POST"]):
+
 	return render_template('home.html')
+@app.route('/register')
+def register():
+	return render_template("register.html")
 
 # TODO: route to /list
 
