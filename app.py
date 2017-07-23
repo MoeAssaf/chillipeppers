65ce7018f61985d14aae8997bc84b3a938e0add4
Moe Assaf <mohammadassaf160@gmail.com>
2017-07-23 18:23:14 +0300

    register AND login CHECK

--- app.py
+++ app.py
@@ -5,14 +5,50 @@ app = Flask(__name__)
 # TODO: connect your database here
 db=dataset.connect("postgres://fbeumtjfoswdzj:77858ede1315dab822e19dad2a8f5979e76c454512737d9598332c145f8ec4ed@ec2-50-19-89-124.compute-1.amazonaws.com:5432/d9kl1is1chajdc")
 
-@app.route('/')
-@app.route('/home')
-def homepage(methods=["POST"]):
 
-	return render_template('home.html')
-@app.route('/register')
+@app.route('/',methods=["GET","POST"])
+@app.route('/home',methods=["GET","POST"])
+def homepage():
+		if request.method == "GET":
+			return render_template('home.html')
+		else:
+			form = request.form
+			username= form["username"]
+			password= form["Password"]
+			contactsTable = db["Contacts"]
+
+			if len(list(contactsTable.find(username = username, password = password))) == 1:
+				return render_template('register.html')
+
+			else:
+				print"YES"
+				return render_template('home.html')
+
+
+@app.route('/register',methods=["POST","GET"])
 def register():
-	return render_template("register.html")
+	if request.method == "GET":
+		return render_template('register.html')
+	else:
+		form = request.form
+		email= form["email"]
+		password= form["Password"]
+		firstname= form["First name"]
+		lastname = form["Last name"]
+		username = form["User name"]
+		country = form["Country"]
+		contactsTable = db["Contacts"]
+		entry = {"email":email, "password":password, "firstname":firstname, "lastname":lastname, "username":username, "country":country}
+		#students names to check
+		if len(list(contactsTable.find(username = username))) == 0:
+			contactsTable.insert(entry)
+			print list(contactsTable.all())
+			return render_template('home.html')
+
+		else:
+			print"YES"
+			return render_template('home.html')
+	
 
 # TODO: route to /list
 
