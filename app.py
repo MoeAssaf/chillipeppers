63ee798f6da7480e9aa55a471944075c58ede87c
Moe Assaf <mohammadassaf160@gmail.com>
2017-07-26 11:30:33 +0300

    Fixed secret key

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import os
 app = Flask(__name__)
 # TODO: connect your database here
 db=dataset.connect("postgres://fbeumtjfoswdzj:77858ede1315dab822e19dad2a8f5979e76c454512737d9598332c145f8ec4ed@ec2-50-19-89-124.compute-1.amazonaws.com:5432/d9kl1is1chajdc")
-app.secret_key=os.urandom(24)
+app.secret_key='\xc35\xd8fT\xa3\xc90\xe5<\xa7!\xef\xaa\x1atV\xc1\d07\xe8\xa7\xbeQt\xb6'
 @app.route('/logout')
 def logout():
 	session.pop("username",None)
@@ -88,6 +88,7 @@ def feed():
 			print list(contactsTable.find(username=c_user))
 			if len(list(contactsTable.find(username = c_user))) >= 1:
 				feed.insert(entry)
+				posts=list(feed.all())[::-1]
 				return render_template('feed.html',posts=posts,c_user=c_user)
 				print entry
 			else:
